95080f5babe8f7a33f61985760c4f7a263af7765
kookong2 <hy010309@naver.com>
2025-01-07 12:39:22 +0900

    [REFACTOR] ngrok 설정

--- src/App.vue
+++ src/App.vue
@@ -114,7 +114,7 @@ onMounted(() => {
     <ChatBotModal
         :isVisible="isChatBotModalVisible"
         @update:isVisible="isChatBotModalVisible = $event"
-        iframeSrc="http://192.168.1.11:8501/"
+        iframeSrc="https://driven-stallion-awake.ngrok-free.app"
     />
   </div>
 </template>
